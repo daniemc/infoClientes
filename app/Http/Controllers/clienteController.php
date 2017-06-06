46b8212a83dbad04379891b71bd342df2d55ede6
MATTELSA\dmenesesc <danielmeneses@mattelsa.net>
2017-06-06 11:56:56 -0500

    ADD - save cliente and return to de view

--- app/Http/Controllers/clienteController.php
+++ app/Http/Controllers/clienteController.php
@@ -5,25 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\GuardarCliente;
 use App\clientes;
+use App\paises;
 class clienteController extends Controller
 {
     public function index()
     {
-        $data['paises'] = [];
-        $data['departamentos'] = [];
-        $data['ciudades'] = [];
+        $data['paises'] = paises::all();
+        
         return view('clientes.index')->with('data', $data);
     }    
 
     public function store(GuardarCliente $form)
     {
-        clientes::create($form);
+        clientes::create([
+            'nit' => bcrypt($form->nit),
+            'nombre' => $form->nombre,
+            'direccion' => $form->direccion,
+            'telefono' => $form->telefono,
+            'pais' => $form->pais,
+            'departamento' => $form->departamento,
+            'ciudad' => $form->ciudad,
+            'cupo' => $form->cupo,
+        ]);
+
+        return $this->index();
     }
 
     public function show()
     {
         $clientes = clientes::all();
-
         return view('clientes.show');   
     }
 }
