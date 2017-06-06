74343ed87c73fef11c9ecf7405dff9ca89a88e04
MATTELSA\dmenesesc <danielmeneses@mattelsa.net>
2017-06-06 11:37:42 -0500

    REMOVE - Jquery script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,6 +73,6 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>-->
-    <script src="https://code.jquery.com/jquery-git.min.js"></script>
+    
 </body>
 </html>
