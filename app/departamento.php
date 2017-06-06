ea8beb54e1eea61cbac955d0a95213325946d734
MATTELSA\dmenesesc <danielmeneses@mattelsa.net>
2017-06-06 09:18:00 -0500

    ADD - relationships to pais and departamento

--- app/departamento.php
+++ app/departamento.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class departamento extends Model
 {
     protected $table = 'departamento';
+
+    public function ciudad()
+    {
+        return $this->hasMany('App\ciudad');
+    }
 }
--- app/pais.php
+++ app/pais.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class pais extends Model
 {
     protected $table = 'pais';
+
+    public function departamentos()
+    {
+        return $this->hasMany('App\departamentos');
+    }
+
+    public function ciudades()
+    {
+        return $this->hasManyThrough('App\departamento', 'App\ciudad');
+    }
 }
