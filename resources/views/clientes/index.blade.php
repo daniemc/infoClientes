79454a97bb86585ccbef143aa10865cd53adc3cb
Soporte Desarrollo <soportedesarrollo@mattelsa.local>
2017-06-07 23:47:32 -0500

    ADD - styles to modal

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -207,7 +207,7 @@
 {!! Charts::assets() !!}
 <!-- Modal -->
 <div class="modal fade" id="visitas" tabindex="-1" role="dialog" aria-labelledby="visitasLabel">
-  <div class="modal-dialog modal-lg" role="document">
+  <div class="modal-dialog modal-lg modal-full-width" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
