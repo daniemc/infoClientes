2a27b9361b6e0244048902b522d68a0518a5d810
MATTELSA\dmenesesc <danielmeneses@mattelsa.net>
2017-06-08 09:51:03 -0500

    ADD - icon brandnav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -21,10 +21,12 @@
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top navbar-inverse">
             <div class="container">
-                <div class="navbar-header">
-
+                <div class="navbar-header center">
+                    
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
+                        <span class="glyphicon glyphicon-info-sign white" ></span>
+                        
                         {{ config('app.name', 'Laravel') }}
                     </a>
                 </div>
