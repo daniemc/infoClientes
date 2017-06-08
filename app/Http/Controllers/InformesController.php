38f7c9011648c79c116b28f5ba57ce89ba130280
Soporte Desarrollo <soportedesarrollo@mattelsa.local>
2017-06-07 21:43:42 -0500

    ADD - chart visitas por ciudad

--- app/Http/Controllers/InformesController.php
+++ app/Http/Controllers/InformesController.php
@@ -4,11 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use DB;
+use Charts;
 class InformesController extends Controller
 {
     
     public function index()
     {
-        return view('informes.general');
+        $datos = DB::table('visitas')
+            ->join('clientes', 'clientes.id', '=', 'visitas.clientes_id')
+            ->join('ciudades', 'ciudades.id', '=', 'clientes.ciudades_id')
+            ->groupBy('ciudades.id', 'ciudades.nombre')
+            ->select(DB::raw('ciudades.id, ciudades.nombre, count(*) as visitas'))
+            ->get();
+
+        $chart = Charts::create('bar', 'material')
+            ->title('Visitas por ciudad')
+            ->template("material")
+            ->elementLabel("Visitas")
+            ->labels($datos->pluck('nombre'))
+            ->values($datos->pluck('visitas'))
+            ->responsive(true);
+
+        return view('informes.general', ['chart' => $chart]);
     }
 }
--- resources/views/informes/general.blade.php
+++ resources/views/informes/general.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-
+{!! Charts::assets() !!}
 <div class="container">
     <div class="row">
         <div class="col-sm-12">
-            
+            {!! $chart->render() !!}
         </div>
     </div>
 </div>
